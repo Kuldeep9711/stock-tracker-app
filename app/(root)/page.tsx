c2c2d77064cb246a556e6c400c32ae4f1a6de7dc
Kuldeep Saini <k72125642@gmail.com>
2025-11-13 00:41:22 +0530

    add stock-heatmap

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,31 @@
-import TradingViewWidget from "@/components/TradingViewWidget"
-import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants"
+import TradingViewWidget from "@/components/TradingViewWidget";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-     <section className="grid w-full gap-8 home-section">
-      <div className="md:col-span-1 xl:col-span-1">
-        <TradingViewWidget 
-          title = "Market Overview"
-           scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
-           config={MARKET_OVERVIEW_WIDGET_CONFIG}
-           className="custom-chart"
-         />
-      </div>
-     </section>
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+        <div className="md-col-span-1 xl:col-span-1">
+           <TradingViewWidget
+            title="Stock Heatmap"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js"
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
