4a1c0f014f73fe59b80cbbe6e276cbff63918f32
Kuldeep Saini <k72125642@gmail.com>
2025-11-15 02:07:44 +0530

    Update InputField component: improve props and UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 const SignUp = () => {
@@ -7,14 +11,47 @@ const SignUp = () => {
     control,
     formState: { errors, isSubmitting },
   } = useForm<SignUpFormData>({
+    defaultValues: {
+    fullName: '',
+    email: '',
+    password: '',
+    country: 'US',
+    investmentGoals: 'Growth',
+    riskTolerance: '',
+    preferredIndustry: 'Technology'
 
-  });
+    },
+    mode: 'onBlur'
+  }, );
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+console.log(data)
+    }catch(e) {
+        console.error(e);
+    }
+  }
+             
   
-  const onSubmit: SubmitHandler<SignInFormData> = (data) => console.log(data);
 
   return (
     <>
       <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form  onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField 
+        name="fullname"
+        label="FULL NAME"
+        placeholder="John Doe"
+        register={register}
+        error={errors.fullName}
+        validation={{ required: 'Full name is required', minLength: 2 }}
+        />
+        
+        <Button type="submit" disabled={isSubmitting} className="yellow-btw w-full mt-5">
+          {isSubmitting ? 'Creating Account' : 'Start Your Investing Journey'}  
+        </Button>
+      </form>
     </>
   );
 };
---
+++ components/forms/InputField.tsx
@@ -0,0 +1,24 @@
+
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
+import { Input } from "../ui/input"
+
+const InputField = ({ name, label, placeholder, type = "text", register, error, validation, disabled, value }: FormInputProps) => {
+  return (
+    <div className="space-y-2">
+        <Label htmlFor={name} className="form-label">
+            {label}
+        </Label>
+        <Input
+        type={type}
+        id={name}
+        placeholder={placeholder}
+        disabled={disabled}
+        value={value}
+        className={cn('form-input', { 'opacity-50 cursor-not-allowed': disabled })}
+        />
+    </div>
+  )
+}
+
+export default InputField
