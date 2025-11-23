1750ebc60bcab4c86b1dea394cef660969d37a0b
Kuldeep Saini <k72125642@gmail.com>
2025-11-23 22:04:18 +0530

    add new feature

--- app/Api/route.ts
+++ app/Api/route.ts
@@ -1,7 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
+import { sendSignUpemail } from "@/lib/inngest/funtions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [sendSignUpemail],
 });
